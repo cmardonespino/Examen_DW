c0a41ab48ca634d2013e1b72763a5e0a223ec279
cmardonespino <maadness.20@gmail.com>
2017-07-06 03:39:39 -0400

    se agrega nueva clase al modelo de la app principal

--- examen/principal/models.py
+++ examen/principal/models.py
@@ -2,5 +2,17 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from sorl.thumbnail import ImageField
 
 # Create your models here.
+
+class Contacto(models.Model):
+       nombre = models.CharField(max_length=200)
+       apellido = models.CharField(max_length=200)
+       Email= models.CharField(max_length=200)
+       Telefono = models.CharField(max_length=200)
+       Direccion = models.CharField(max_length=200)
+       imagen = models.ImageField(upload_to='img')
+
+       def __str__(self):
+              return (self.nombre)
